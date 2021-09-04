3a47f70ed42a16edcbcfe8a1589cc4dc3580259e
Sergey <piolersa85@mail.ru>
2021-09-04 15:56:13 +0200

    operand

--- Closures/ViewController.swift
+++ Closures/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    func freeMan(parameter: String) { //функция ничего не возвращает (String) -> () или  (String) -> Void(пустота)
+        
+    }
+    
+    func changeSign(operand: Double) -> Double { // меняем знак чиста double на минус changeSign(поменять знак) operand(объект действия)
+        return -operand
+    }
+    
+    
+    
+    
+    
+  //  var operarion: (Double) -> Double
+  //  operation = sqrt // sqrt - квадратный корень
+  // operation(9) // возвращается  3
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
+    
+    
 }
 
