d42a4e2ccfe38fd3d3f70a3544acb1628f80c781
Sergey <piolersa85@mail.ru>
2021-09-04 13:52:20 +0200

    Initial Commit

---
+++ Closures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Closures
+//
+//  Created by Sergey on 04.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
