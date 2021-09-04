ab6e7be2e4d27c5800f79f629b42b08844283c5d
Sergey <piolersa85@mail.ru>
2021-09-04 16:42:07 +0200

    array

--- Closures/ViewController.swift
+++ Closures/ViewController.swift
@@ -17,29 +17,59 @@ class ViewController: UIViewController {
     func changeSign(operand: Double) -> Double { // меняем знак чиста double на минус changeSign(поменять знак) operand(объект действия)
         return -operand
     }
-    
-    
-    
-    
-    
   //  var operarion: (Double) -> Double
   //  operation = sqrt // sqrt - квадратный корень
   // operation(9) // возвращается  3
     
     
+                                       // Замыкания(Closures)
+    
+//  1.  func changeSign(operand: Double) -> Double { return -operand }
+    
+ //  2. var operation = (operand: Double) -> Double { return -operand }
+    
+    var operation = {(operand: Double) -> Double in return -operand } // готовое замыкание самый длинный вариант сокращаем его
+    
+    // swift может понимать тип из контекста
+    
+    var operation1 = {(operand: Double)  in return -operand }
+    
+    var operation2 = {(operand: Double) in return -operand }
+    
+    var operation3 = {(operand: Double) in -operand }
+    
+    // заменяем название аргумента operand
+     
+ //   var operation4 =  { -$0 } // выдает ошибку значения Double не видит
     
     
     
+                        // Замыкание в качестве аргументов
     
-    
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       
+    func someFunction(someArray:[Int], someNumber: Int) -> [Int] {
+        
+         return someArray
     }
-
+    
+    
+    
+    
+    func someFunction1(someArray:[Int], myFunction: (Int) -> Bool) -> [Int] {// объясняем функции что ей делать помогая вторым параметром
+        
+        return someArray
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
 
     
     
